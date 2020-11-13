d8f71593cd5241a0a78ffde8484476ffc834563b
caixian <qq52577517@163.com>
2020-11-13 09:19:37 +0800

    class2

--- server/src/resolves/UserResolves.ts
+++ server/src/resolves/UserResolves.ts
@@ -1,6 +1,14 @@
-import { hash } from "bcryptjs";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { Arg, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from '../entity/User'
+
+@ObjectType()
+class LoginReponse {
+
+    @Field()
+    accessToken: String
+}
 
 @Resolver()
 export class UserResolves {
@@ -31,6 +39,26 @@ export class UserResolves {
         }
 
         return true;
+    }
+
+    @Mutation(() => LoginReponse)
+    async login(
+        @Arg('email', () => String) email: string,
+        @Arg('password', () => String) password: string,
+    ): Promise<LoginReponse> {
+        const user = await User.findOne({where: { email }});
+        if (!user) {
+            throw new Error("invalid email");
+        }
+
+        const validPwd = await compare(password, user.password);
+        if (!validPwd) {
+            throw new Error("invalid password");
+        }
+
+        return {
+            accessToken: sign({userId: user.id}, '73454593ksfsd', {expiresIn: '15m'})
+        };
     }
     
 }
