e3e9508d49c791104eaa99ed911f4c66b1670943
caixian <qq52577517@163.com>
2020-11-17 00:24:29 +0800

    class 4

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -3,27 +3,31 @@ import { sign } from "jsonwebtoken";
 import { User } from "./entity/User";
 
 interface MyPlayload {
-    userId: number,
-    tokenVersion?: number,
+  userId: number;
+  tokenVersion?: number;
 }
 
 const createAccessToken: (T: User) => string = (user) => {
-    const playload: MyPlayload = {userId: user.id, tokenVersion: user.tokenVersion};
-    return sign(playload, process.env.ACCESS_TOKEN_SALT!, {expiresIn: '15m'})
-}
+  const playload: MyPlayload = {
+    userId: user.id,
+    tokenVersion: user.tokenVersion,
+  };
+  return sign(playload, process.env.ACCESS_TOKEN_SALT!, { expiresIn: "15s" });
+};
 
 const createRefreshToken: (T: User) => string = (user) => {
-    const playload: MyPlayload = {userId: user.id, tokenVersion: user.tokenVersion};
-    return sign(playload, process.env.REFRESH_TOKEN_SALT!, {expiresIn: '7d'})
-}
+  const playload: MyPlayload = {
+    userId: user.id,
+    tokenVersion: user.tokenVersion,
+  };
+  return sign(playload, process.env.REFRESH_TOKEN_SALT!, { expiresIn: "7d" });
+};
 
 const sendRefreshToken: (T: User, res: Response) => void = (user, res) => {
-    res.cookie('jid', createRefreshToken(user), {httpOnly: true});
-}
-
-export {
-    MyPlayload,
-    createAccessToken,
-    createRefreshToken,
-    sendRefreshToken,
+  res.cookie("jid", createRefreshToken(user), {
+    httpOnly: true,
+    path: "/refresh_token",
+  });
 };
+
+export { MyPlayload, createAccessToken, createRefreshToken, sendRefreshToken };
